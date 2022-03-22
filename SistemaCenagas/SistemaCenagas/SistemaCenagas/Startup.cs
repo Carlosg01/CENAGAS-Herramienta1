5db3cfe2671a3decc7cf3af4c0c3630fc5e5fe22
armando1530343 <armandohdz1530343@gmail.com>
2022-03-22 02:07:44 -0600

    22/Marzo/2022

--- SistemaCenagas/SistemaCenagas/SistemaCenagas/Startup.cs
+++ SistemaCenagas/SistemaCenagas/SistemaCenagas/Startup.cs
@@ -43,7 +43,7 @@ namespace SistemaCenagas
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -52,7 +52,11 @@ namespace SistemaCenagas
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            }*/
+
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
